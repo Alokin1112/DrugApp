e409d27dbafcc921f78242ecb189e6d46d3af9a0
Alokin1112 <davidson1@o2.pl>
2020-10-08 20:36:47 +0200

    Added Daily Use Calc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import styled,{createGlobalStyle} from 'styled-components';
+import {MaxUseCalc} from './components/ingredients'
+const GlobalStyle=createGlobalStyle`
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+    padding:0;
+    background-color:#fff;
+    color:#4a4a4a;
+    overflow-x:hidden;
+    font-size:1.5rem;
+  }
+`;
+const Main=styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  padding: 3em;
+  display: flex;
+  justify-content: center;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle/>
+      <Main>
+        <MaxUseCalc dailyUse={5}/>
+      </Main>
+      </>
   );
 }
 
---
+++ src/components/ingredients/MaxUseCalc.js
@@ -0,0 +1,71 @@
+import React,{useState} from 'react';
+import styled from 'styled-components';
+
+const CalcWrapper=styled.div`
+  box-shadow: 0 0.5em 1em -0.125em rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.02);
+  background-color: #fff;
+  color: #4a4a4a;
+  min-width: 40%;
+  max-width: 60%;
+  border-radius: 6px;
+  padding: 1.25em;
+  min-height: 20vh;
+  max-height: 45vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+const CalcInput=styled.input`
+  width: 70%;
+  background-color: #fff;
+  border-color: #dbdbdb;
+  color: #363636;
+}
+`
+const Disabled=styled.button`
+  background-color: #f5f5f5;
+  border-color: #dbdbdb;
+  color: #7a7a7a;
+  box-shadow: none;
+  pointer-events: none;
+`
+const AlignLeft=styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction:row;
+  width: 100%;
+`
+const Daily=styled.p`
+  font-size: 0.75em;
+  border: 1px solid #4a4a4a;
+  padding: 0.1em;
+  border-radius: 4px;
+`
+const Title=styled.h3`
+  margin: 0.25em;
+  padding: 0;
+  font-size: 0.75em;
+`
+function MaxUseCalc({dailyUse}){
+
+  const[weight,setWeight]=useState("");
+  const handleChange=(e)=>{
+    const{value}=e.target;
+    setWeight(value);
+  }
+  return(
+    <CalcWrapper>
+      <Title>Dzienne zapotrzebowanie</Title>
+      <AlignLeft>
+        <CalcInput type="number" min="1" max="660" placeholder="Wpisz Swoją Wagę" value={weight} onChange={handleChange}/>
+        <Disabled disabled>Kg</Disabled>
+      </AlignLeft>
+      {weight*dailyUse>0?<Daily>{weight*dailyUse} mg</Daily>:null}
+
+    </CalcWrapper>
+  );
+}
+
+export default MaxUseCalc;
