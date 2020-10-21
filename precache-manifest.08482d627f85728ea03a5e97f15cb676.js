4d665b510ac2e6cf1a6ec6b26c68e9d3cbf0e8d4
Alokin1112 <davidson1@o2.pl>
2020-10-21 18:22:12 +0200

    Updates

---
+++ precache-manifest.08482d627f85728ea03a5e97f15cb676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0adcc4add685b8f79525967488acb6c",
+    "url": "/DrugApp/index.html"
+  },
+  {
+    "revision": "28a3f4067adc1738b786",
+    "url": "/DrugApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f20fadbc26e381c4a271",
+    "url": "/DrugApp/static/js/2.640d321e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DrugApp/static/js/2.640d321e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a3f4067adc1738b786",
+    "url": "/DrugApp/static/js/main.10b9608b.chunk.js"
+  },
+  {
+    "revision": "bc902fffc0b30da838d6",
+    "url": "/DrugApp/static/js/runtime-main.ced89ce8.js"
+  }
+]);
